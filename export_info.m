68e70cae2e43ce68b2ced07514f8b3ea69464a1b
Gio <github@gpiantoni.com>
2012-05-09 12:06:40 +0200

    info in csv about sw detection

---
+++ export_info.m
@@ -0,0 +1,17 @@
+function [output] = export_info(cfg)
+%EXPORT_INFO: export info on slow wave detection for hgse
+
+output = '';
+output = [output sprintf(' %d', cfg.redefsw.stage) ','];
+output = [output sprintf(' %d,', cfg.redefsw.rejart)];
+
+output = [output sprintf(' %s, %1.f,', cfg.redefsw.sw.preproc.lpfilter, cfg.redefsw.sw.preproc.lpfreq)];
+output = [output sprintf(' %s, %1.f,', cfg.redefsw.sw.preproc.hpfilter, cfg.redefsw.sw.preproc.hpfreq)];
+
+output = [output sprintf(' %d,', cfg.redefsw.sw.negthr)];
+output = [output sprintf(' %d,', cfg.redefsw.sw.p2p)];
+output = [output sprintf(' %1.f-%1.f,', cfg.redefsw.sw.zcr(1), cfg.redefsw.sw.zcr(2))];
+output = [output sprintf(' %d,', cfg.redefsw.sw.zeropad)];
+
+output = [output sprintf(' %s,', cfg.redefsw.event)];
+output = [output sprintf(' %d,', cfg.redefsw.dur)];
